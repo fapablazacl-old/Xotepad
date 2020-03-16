fa19d225f4fc91ee4ee88066012eb9a33a50fc6c
Felipe Apablaza <ing.apablaza@gmail.com>
2020-03-16 00:32:10 -0300

    Minor comment

--- component/Xotepad/sources/MainWindowModel.hpp
+++ component/Xotepad/sources/MainWindowModel.hpp
@@ -1,12 +1,14 @@
 
 #pragma once
 
+/*
+The model is implicit inside the variables of the MainWindowPresenter  ...
 #include <optional>
 #include <string>
 #include <fstream>
-
 
 class MainWindowModel {
 public:
 
 };
+*/
