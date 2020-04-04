4dfa817adc9fca134d83ec021e04934477590bf4
Felipe Apablaza <ing.apablaza@gmail.com>
2020-04-04 10:37:36 -0300

    [Win32] Added Find & Replace dialog initial design

---
+++ component/Xotepad/sources/resource.h
@@ -0,0 +1,28 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+//
+#define IDD_FORMVIEW                    101
+#define IDC_CHECK1                      1000
+#define IDC_CHECK2                      1001
+#define IDC_CHECK3                      1002
+#define IDC_EDIT1                       1003
+#define IDC_EDIT2                       1004
+#define IDC_BUTTON1                     1006
+#define IDC_BUTTON2                     1007
+#define IDC_BUTTON4                     1008
+#define IDC_BUTTON3                     1009
+#define IDC_RADIO1                      1010
+#define IDC_RADIO2                      1011
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1012
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
